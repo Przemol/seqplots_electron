f9b76f1659e06dec96ae3dad084896f92dbb7904
Przemol <pstempor@gmail.com>
2016-10-11 17:09:03 +0100

    Bundle structure reorganization for electron-builder pt2

--- preload.js
+++
@@ -1,7 +0,0 @@
-//window.$ = window.jQuery = require('/path/to/jquery');
-document.addEventListener("DOMNodeInserted", function(event) {
-    if (!!window && !(!!window.$)) {
-        window.$ = window.jQuery = require('jquery');
-    }
-    const {ipcRenderer} = require('electron');
-});
